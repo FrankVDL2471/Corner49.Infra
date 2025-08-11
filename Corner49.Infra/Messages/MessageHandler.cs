d74c6fbdf02d49c94e52f94c2498ad8b90a11768
Frank Vanderlinden <frankvdl@gmail.com>
2025-08-11 11:11:04 +0200

    improve message handler

--- Corner49.Infra/Messages/MessageHandler.cs
+++ Corner49.Infra/Messages/MessageHandler.cs
@@ -1,4 +1,5 @@
 ﻿using Corner49.Infra.ServiceBus;
+using System.Security.Cryptography;
 
 namespace Corner49.Infra.Messages {
 
@@ -10,11 +11,14 @@ namespace Corner49.Infra.Messages {
 		public virtual async Task MessageReceived(ServiceBusCommand cmd) {
 			object msg = cmd.GetData() ?? cmd.GetData<T>();
 
-			await Process(cmd.Name, (T)msg);
+			await Process(cmd.Name ?? cmd.GetType().Name, (T)msg);
 
-			var method = GetType().GetMethod(cmd.Name, new Type[] { msg.GetType() });
+			var method = GetType().GetMethod(cmd.Name ?? cmd.GetType().Name, new Type[] { msg.GetType() });
 			if (method != null) {
-				await (Task)method.Invoke(this, new object[] { msg });
+				var task = method.Invoke(this, new object[] { msg }) as Task;
+				if (task != null) {
+					await task.ConfigureAwait(false);	
+				}
 			}
 		}
 
